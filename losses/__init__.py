da0c26270053c80a1d45a54a353710a42a2447fa
vltanh <vltheanh030698@gmail.com>
2020-04-02 13:56:14 +0700

    sum of multiple loss functions with weights

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,3 +1,4 @@
 from .crossentropy import BCEWithLogitsLoss, WeightedBCEWithLogitsLoss, CrossEntropyLoss
 from .focalloss import FocalLoss
 from .diceloss import DiceLoss
+from .mixedloss import MixedLoss
